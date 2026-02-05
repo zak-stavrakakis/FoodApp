085b1915fa185c59f109a23dd631fdbe52650db6
stavrakakis.z <stavrakakis.z@gmail.com>
2026-02-05 21:46:49 +0200

    cart slices set before connect it with the components

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
-import App from './App.jsx'
-import './index.css'
+import App from './App.jsx';
+import store from './redux-store/index.js';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>,
+);
